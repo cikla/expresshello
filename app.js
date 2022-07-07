41b37e018472e5c4a20ce90890e621e364576d72
Mert Çıklabakkal <mert.ciklabakkal@eriateknoloji.com>
2022-07-07 09:57:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello Cikla!</h1>');
+});
+
+app.get('/healthcheck', function (req, res) {
+  res.send('<h1>I am healty!</h1>');
+});
+
+app.listen(3000, function () {
+  console.log('Listening to Port 3000');
+});
